618a598858a090de1032c43e7856fb8ae0e756f7
Harold Abejaron <habejaron@gmail.com>
2020-01-11 13:15:27 +0800

    Number 2 Function has been added.

--- Calcu/Calcu/Form1.vb
+++ Calcu/Calcu/Form1.vb
@@ -6,4 +6,12 @@
             monitor.Text = monitor.Text & "1"
         End If
     End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If monitor.Text = "0" Then
+            monitor.Text = "2"
+        Else
+            monitor.Text = monitor.Text & "2"
+        End If
+    End Sub
 End Class
